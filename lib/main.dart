d1537401647e48e55bb817c5d2a9bd1ceaf4b328
salihyildirim <SalihYildirimCE@gmail.com>
2022-03-02 02:46:17 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const BenimUygulamam());
 }
-//Falci uygulamasi olusturuldu !
+
 class BenimUygulamam extends StatelessWidget {
   const BenimUygulamam({Key? key}) : super(key: key);
 
